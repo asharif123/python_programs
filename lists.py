f9b95950e8a73002c76978df487cbd977497ba70
asharif123 <awadsharif9@gmail.com>
2022-11-19 21:11:16 -0800

    lists.py

---
+++ lists.py
@@ -0,0 +1,26 @@
+##lists
+count = [1,2,3,4,5]
+change = [1, "pennies", 2, 'dimes', 3, 'quarters']
+for number in count:
+    print("This is the count: %s" %(number))
+
+numbers = []
+for i in range(5):
+    numbers.append(i)
+# print(numbers)
+
+##2D array
+twod_array = [[1,2,3],[4,5,6]]
+for list in twod_array:
+    for number in list:
+        print(number)
+
+#while loop
+i = 0
+numbers = []
+while i < 6:
+    print("The new i is %s" %(i))
+    i += 1
+
+
+
