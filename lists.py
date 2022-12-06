5b860f73bf2a891684b6e8f87bfa1029de64a0bb
asharif123 <awadsharif9@gmail.com>
2022-12-06 10:47:36 -0800

    updated max numbers

--- lists.py
+++ lists.py
@@ -2,7 +2,7 @@
 ##find largest number in a list
 numbers = [35,2,4,12,-2,-3,456,44,50,10]
 max_value = numbers[0]
-for i in range(len(numbers)):
+for i in range(1,len(numbers)):
     if (max_value < numbers[i]):
         max_value = numbers[i]
 print(max_value)
@@ -17,5 +17,16 @@ print(matrix[2][0])
 numbers = [35,2,4,12,-2,-3,456,44,50,10]
 #pass index and what to add
 numbers.insert(4,"240")
-numbers.remove("240")
+numbers.remove("240 ")
 print(numbers)
+
+#removes last item in list
+numbers.pop()
+##empties our list
+# numbers.clear()
+
+##returns index of first occurence of item
+numbers.index(-3)
+
+##count number of occurances
+numbers.count(2)
