400d1cb5f19addca293748f39fff13ea30b7a76e
asharif123 <awadsharif9@gmail.com>
2022-12-06 23:05:50 -0800

    classes

--- classes.py
+++ classes.py
@@ -2,40 +2,75 @@
 
 ##A class inherits from the class named object to make a class
 ##assume that Python always requires (object) when you make a class
-class Animal(object):
-    pass
+
+class Point:
+    #consttuctor, use init method to initialize our objects
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+    def move(self):
+        print("Moved!")
+
+    def draw(self):
+        print("Draw!")
+
+point1 = Point(10,20)
+print(point1.x)
+print(point1.y)
+point1.draw()
+point1.move()
 
 
-##Dog class is inherited from Animal
-class Dog(Animal):
+##define person object having name attribute and talk method
+class Person:
     def __init__(self,name):
         self.name = name
+    
+    def talk(self):
+        print("My name is %s!" %(self.name))
+
+awad = Person("Awad")
+print(awad.name)
+awad.talk()
+
+#diff object of instance from point 1
+# point2 = Point()
+
+# class Animal(object):
+#     pass
 
 
-class Cat(Animal):
-    def __init__(self,name):
-        self.name = name
-
-class Person(object):
-    def __init__(self,name):
-        self.name = name
-
-        ##person has some kind of pet
-        self.pet = None
-
-class Employee(Person):
-    def __init__(self,name,salary):
-##used to inherit attributes from parent class
-#That’s how you can run the __init__ method of a parent class reliably.
-        super(Employee,self).__init__(name)
-        self.salary = salary
+# ##Dog class is inherited from Animal
+# class Dog(Animal):
+#     def __init__(self,name):
+#         self.name = name
 
 
-class Fish(object):
-    pass
+# class Cat(Animal):
+#     def __init__(self,name):
+#         self.name = name
 
-frank = Employee('Frank',120000)
-print(frank.name)
+# class Person(object):
+#     def __init__(self,name):
+#         self.name = name
 
-fido = Dog("fido")
-print(fido.name)
+#         ##person has some kind of pet
+#         self.pet = None
+
+# class Employee(Person):
+#     def __init__(self,name,salary):
+# ##used to inherit attributes from parent class
+# #That’s how you can run the __init__ method of a parent class reliably.
+#         super(Employee,self).__init__(name)
+#         self.salary = salary
+
+
+# class Fish(object):
+#     pass
+
+# frank = Employee('Frank',120000)
+# print(frank.name)
+
+# fido = Dog("fido")
+# print(fido.name)
