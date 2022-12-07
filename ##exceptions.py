a9704d6bd2609a80105cc24475e19291f8fa620c
asharif123 <awadsharif9@gmail.com>
2022-12-06 22:40:20 -0800

    exception handling

---
+++ ##exceptions.py
@@ -0,0 +1,11 @@
+##exceptions
+try:
+
+    age = int(input("Age: "))
+    income = 20000
+    risk = income / age
+    print(age)
+except ZeroDivisionError:
+    print("Age cannot be zero!")
+except ValueError:
+    print("Invalid value!")
